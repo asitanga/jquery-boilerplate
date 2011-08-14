e487648b252aefc0b07b888102822866a0390ece
Zeno Rocha <root@zeno.(none)>
2011-08-14 16:14:29 -0300

    now passing on JSLint

--- jquery.boilerplate.js
+++ jquery.boilerplate.js
@@ -1,27 +1,16 @@
 // remember to change every instance of "pluginName" to the name of your plugin!
-// the semicolon at the beginning is there on purpose in order to protect the integrity of your scripts when
-// mixed with incomplete objects, arrays, etc.
-;(function($) {
+(function ($) {
 
-	$.fn.pluginName = function(options) {  
+	$.fn.pluginName = function (options) {
 
 		// plugin's default options
 		// this is private property and is accessible only from inside the plugin
 		var defaults = {
-			propertyName : 'value',
-    		};
+			propertyName : 'value'
+		};
 
 		// lets merge options with default settings
-		options = $.extend( {}, defaults, options );
-
-		// maintaining chainability
-		return this.each(function() {
-
-			var $this = $( this );
-
-			// your code here
-
-		});
+		options = $.extend({}, defaults, options);
 
 		// private function for debugging
 		var debug = function ($obj) {
@@ -30,8 +19,17 @@
 			}
 		};
 
+		// maintaining chainability
+		return this.each(function () {
+
+			var $this = $(this);
+
+			// your code here
+
+		});
+
 	};
 
-})(jQuery);
+}(jQuery));
 
 
