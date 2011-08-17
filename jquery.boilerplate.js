c03594bb67b183a12e8cbe0151807fc1fc1f984d
Zeno Rocha <zno.rocha@gmail.com>
2011-08-17 20:45:29 -0300

    code passed on JSHint

--- jquery.boilerplate.js
+++ jquery.boilerplate.js
@@ -1,7 +1,7 @@
-// remember to change every instance of "pluginName" and "pluginNamespace"
-(function($){
+// remember to change every instance of "pluginName"
+(function ($) {
 
-    $.pluginName = function(element, options){
+    $.pluginName = function (element, options) {
 
         // To avoid scope issues, use 'self' instead of 'this'
         // to reference this class from internal events and functions.
@@ -14,34 +14,35 @@
         // Add a reverse reference to the DOM object
         self.$element.data('pluginName', self);
         
-        self.init = function(){
+        self.init = function () {
             self.options = $.extend({}, $.pluginName.defaultOptions, options);
             
             // Put your initialization code here
         };
         
         // Sample Function, Uncomment to use
-        // self.functionName = function(paramaters){
+        // self.functionName = function (paramaters) {
         // 
         // };
         
         // Run initializer
         self.init();
+
     };
     
     $.pluginName.defaultOptions = {
         propertyName: 'value'
     };
     
-    $.fn.pluginName = function(options){
-        return this.each(function(){
+    $.fn.pluginName = function (options) {
+        return this.each(function () {
             (new $.pluginName(this, options));
         });
     };
     
     // This function breaks the chain, but returns
     // the pluginName if it has been attached to the object.
-    $.fn.getpluginName = function(){
+    $.fn.getPluginName = function () {
         this.data('pluginName');
     };
     
